3fb0e83a1c1e5717e3a36e76cadf3934d00136bc
manju <manjunatha3800@gmail.com>
2023-07-12 16:22:03 +0000

    solution

---
+++ index.js
@@ -0,0 +1,36 @@
+let allColorContent = document.getElementById("colorPickerContainer");
+let selectedColorHe = document.getElementById("selectedColorHexCode");
+
+function color_1() {
+  const color_to_be_change = document.getElementById("button1").textContent;
+
+  selectedColorHe.textContent = color_to_be_change;
+  allColorContent.style.backgroundColor = color_to_be_change;
+}
+
+function color_1() {
+  const color_to_be_change = document.getElementById("button1").textContent;
+
+  selectedColorHe.textContent = color_to_be_change;
+  allColorContent.style.backgroundColor = color_to_be_change;
+}
+
+function color_2() {
+  const color_to_be_change = document.getElementById("button2").textContent;
+
+  selectedColorHe.textContent = color_to_be_change;
+  allColorContent.style.backgroundColor = color_to_be_change;
+}
+
+function color_3() {
+  const color_to_be_change = document.getElementById("button3").textContent;
+
+  selectedColorHe.textContent = color_to_be_change;
+  allColorContent.style.backgroundColor = color_to_be_change;
+}
+function color_4() {
+  const color_to_be_change = document.getElementById("button4").textContent;
+
+  selectedColorHe.textContent = color_to_be_change;
+  allColorContent.style.backgroundColor = color_to_be_change;
+}
